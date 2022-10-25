eec1ab48456658550a1f3bbb8f985361bc5d3cfe
AsifKarim7 <asifkarim275@gmail.com>
2022-10-25 13:39:15 +0600

    Header done

--- src/Pages/Courses/Courses.js
+++ src/Pages/Courses/Courses.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import LeftSideNav from '../Shared/LeftSideNav/LeftSideNav';
+
 
 const Courses = () => {
     return (
         <div>
             <h1>Courses</h1>
+            <LeftSideNav></LeftSideNav>
         </div>
     );
 };
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import { Link } from 'react-router-dom';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 const Header = () => {
     return (
-        <div>
-            <h1>This is Header</h1>
-        </div>
+        <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+            <Container>
+                <Navbar.Brand><Link className='text-decoration-none fw-bold' to='/'>Code Adaptive</Link></Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link><Link className='text-decoration-none' to='/'>Home</Link></Nav.Link>
+                        <Nav.Link><Link className='text-decoration-none' to='/courses'>Courses</Link></Nav.Link>
+                        <Nav.Link><Link className='text-decoration-none' to='/faq'>FAQ</Link></Nav.Link>
+                        <Nav.Link><Link className='text-decoration-none' to='/blog'>Blog</Link></Nav.Link>
+                    </Nav>
+                    <Nav>
+                        <Nav.Link><Link className='text-decoration-none' to='/login'>Login</Link></Nav.Link>
+                        <Nav.Link><Link className='text-decoration-none' to='/register'>Register</Link></Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 };
 
